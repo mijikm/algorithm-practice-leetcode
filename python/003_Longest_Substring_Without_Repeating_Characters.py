549590aef65e47bae08e6574fe7afa5f290ba534
Miji Kim <shelbkim@gmail.com>
2020-07-25 17:02:00 +1000

    Add files via upload

--- python/003_Longest_Substring_Without_Repeating_Characters.py
+++ python/003_Longest_Substring_Without_Repeating_Characters.py
@@ -1,25 +1,38 @@
 class Solution(object):
+    # 1. Approach: Count only indexes
+    # - Find last occurrence of the char, and calculate length of the string
+    #   between current pointer and last occurrence of the char + 1
+
+    # Time Complexity: O(n)
+    # assume that n represents the length of the string "s"
+
+    # Space Complexity: O(1)
+    # since it is limited to only 26+ chars
+
     def lengthOfLongestSubstring(self, s):
         """
         :type s: str
         :rtype: int
         """
-        hash_table = {}
-        my_list = []
-        sub_length = 0
 
-        for i in range(len(s)):
-            if s[i] not in my_list:
-                my_list.append(s[i])
-                sub_length = len(my_list)
-            else:
-                hash_table[str(my_list)] = sub_length
-                my_list = []
-                sub_length = 0
-                my_list.append(s[i])
-                sub_length = len(my_list)
+        left_valid_letter = 0
+        longest = 0
+        last_seen = {}
 
-        if hash_table is not None:
-            return max(hash_table.values())
-        else:
-            return sub_length
+        for i, letter in enumerate(s):
+            if letter in last_seen:
+                # left_valid_letter must be greater than any position which is seen again
+                # left_valid_letter is 1 index greater than the last seen letter's index
+                left_valid_letter = max(left_valid_letter, last_seen[letter] + 1)
+
+            # update the dictionary with the letter and the index
+            last_seen[letter] = i
+
+            # Length of substring from left_valid to i is "i - left_most_valid + 1"
+            longest = max(longest, i - left_valid_letter + 1)
+
+        return longest
+
+
+
+
