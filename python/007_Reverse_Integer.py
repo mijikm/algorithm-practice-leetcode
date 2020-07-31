9e1b45381dea2bc693e5eb89e5e12ecb2efac862
Miji Kim <shelbkim@gmail.com>
2020-08-01 00:17:14 +1000

    Add files via upload

---
+++ python/007_Reverse_Integer.py
@@ -0,0 +1,36 @@
+def reverse(x):
+    """
+    :type x: int
+    :rtype: int
+    """
+    # Approach 1: Check negative and validate
+    # Time Complexity: O(n)
+    # where n == len(s)
+    # Space Complexity: O(1)
+
+    # set default neg value to False
+    neg = False
+    # if negative value
+    if x < 0:
+        neg = True
+        # convert to positive value
+        x = x * -1
+    # reverse string
+    s = str(x)[::-1]
+    # convert to integer
+    n = int(s)
+
+    # negative value
+    if neg == True and abs(n) < 2 ** 31 - 1:
+        # convert to original negative value
+        return n * -1
+    # positive value
+    elif abs(n) < 2 ** 31 - 1:
+        return n
+    # not 32-bit signed integer range
+    else:
+        return 0
+
+if __name__ == '__main__':
+    solution = reverse(-2148)
+    print(solution)
