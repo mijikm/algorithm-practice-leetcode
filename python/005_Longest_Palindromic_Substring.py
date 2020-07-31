e95ba738c11927225acae72dd2db4db0208d9dbb
Miji Kim <shelbkim@gmail.com>
2020-07-31 19:02:43 +1000

    Add files via upload

--- python/005_Longest_Palindromic_Substring.py
+++ python/005_Longest_Palindromic_Substring.py
@@ -4,20 +4,49 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
+    # Approach 1: Brute Force
+    # Time Complexity: O(n^3)
+    # assume n is the length of the input string
+    # verifying each substring takes O(n) time
+    # Space Complexity: O(1)
+
+    #    if len(s) <= 1 or s == s[::-1]:
+    #        return s
+    #    longest = ""
+
+    #    for i in range(len(s)):
+    #        for j in range(len(s), i, -1):
+    #            if s[i:j] == s[i:j][::-1]:
+    #                if len(s[i:j]) > len(longest):
+    #                    longest = s[i:j]
+    #    return longest
+
+    # Approach 2: Expand around center
+    # Time Complexity: O(n^2)
+    # assume n is the length of the input string
+    # as expanding a palindrome around its center could take O(n) time, the overall complexity is O(n^2).
+    # Space Complexity: O(1)
         if len(s) <= 1 or s == s[::-1]:
             return s
-
-        start = 0
-        max_length = 1
+        longest = ""
 
         for i in range(len(s)):
-            odd = s[i - max_length - 1: i + 1]
-            even = s[i - max_length: i + 1]
-            if i - max_length - 1 >= 0 and odd == odd[::-1]:
-                start = i - max_length - 1
-                max_length += 2
-            elif i - max_length >= 0 and even == even[::-1]:
-                start = i - max_length
-                max_length += 1
+            # odd case
+            tmp = helper(s,i,i)
+            if len(tmp) > len(longest):
+                # update longest
+                longest = tmp
 
-        return s[start: start + max_length]
+            # even case
+            tmp = helper(s,i,i+1)
+            if len(tmp) > len(longest):
+                longest = tmp
+
+        return longest
+
+def helper(s,l,r):
+    while l >= 0 and r < len(s) and s[l] == s[r]:
+        l -= 1 # decrement the left
+        r += 1 # increment the right
+
+    return s[l+1:r]
